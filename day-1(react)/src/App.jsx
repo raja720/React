96054bf365197f3a34c15748fc201cc0d7fd9af2
raja720 <rajababu402003@gail.com>
2025-05-11 09:52:39 +0530

    how to add oncilk method

--- day-1(react)/src/App.jsx
+++ day-1(react)/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Fragment } from 'react'
 
 const App = () => {
+
+  //here we write js logic
+
+  let eventhandler = ()=>{
+    alert("hello madarchod")
+  }
+
+  const eventparamhandler = (msg)=>{
+    alert(msg)
+  }
+
+  
+  
   return (
-    <div>App</div>
-  )
+    <>
+      <div>raja ki aayi hai baaraat</div>
+      <button onClick={eventhandler}>click</button>
+      <button onClick={ ()=>eventparamhandler("i am msg from bujh gye diye")}>click(param)</button>
+      </>
+  );
 }
 
 export default App
+
+// a function component will always return html
+
+// we can not write anything after return
+// we can only return single data/ entitu/ variable/ value
+
+// there must be single return in a function and that must in the last statment
